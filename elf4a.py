b42d6f184db2b40efc3540078d2c916d686c0863
gsaini <46835538+gsaini26@users.noreply.github.com>
2022-12-21 15:12:20 -0800

    adding 4a

--- elf4a.py
+++ elf4a.py
@@ -1,10 +1,30 @@
-f = open("mini4a.txt",'r')
-firstRange = f.readline().strip()
-twoItems = firstRange.split(',')
-firstNum = twoItems[0].split('-')
-secondNum = twoItems[1].split('-')
+import re
 
-print(firstNum, " ", secondNum, " ")
+f = open("input4a.txt",'r')
 
+count=0
+for line in f:
 
+    firstLine = line.strip()
+    allItems = re.split(r'-|,', firstLine)
+    print(allItems)
+    # we can assume exactly 4 items in this problem
+    
+    if ((allItems[3]>=allItems[0]>=allItems[2]) and (allItems[2]<=allItems[1]<=allItems[3])) or ((allItems[0]<=allItems[2]<=allItems[1]) and (allItems[0]<=allItems[3]<=allItems[1])):
+        print("Inrange")
+       
+        count=count+1
+    else:
+        print("notInRange")
+print("total",count)
+# if the first number in the second range is in the first range or the second 
+# number in the first range is in the second range,
+# there are intersecting numbers
+
+# if the first number is in the second range and the second number is in the second range
+# or if the second number is in the first range and the second number is in the first range
+# then the number is fully contained in the other. First Part of 4.
+f.close()
+
+    
 
